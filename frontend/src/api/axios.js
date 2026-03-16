4d5001fdd40604f0e7a07a1d9a2c0eecd2d0047d
Sejal Anuja <anujasejal@gmail.com>
2026-03-16 21:47:45 +0530

    update api url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://placement-management-system-77mf.onrender.com/api",
   withCredentials: true,  // backend URL
 });
 
